fa349fa48174b6ab1b09f0e833510ebe00126dc9
Thomas Bartlett <116368168+thom-bartlett@users.noreply.github.com>
2024-08-30 10:40:35 -0400

    Update globalprotect.sh

--- fragments/labels/globalprotect.sh
+++ fragments/labels/globalprotect.sh
@@ -1,11 +1,10 @@
 globalprotect)
     name="globalprotect"
     type="pkg"
-    appNewVersion=$(curl -fs https://pan-gp-client.s3.amazonaws.com/ | xpath '//ListBucketResult/Contents[last()]/Key' 2>/dev/null | sed -n 's/.*<Key>\([0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\)-.*/\1/p')
+    appNewVersion=$(curl -fs https://pan-gp-client.s3.amazonaws.com/ | xpath '//ListBucketResult/Contents/Key[contains(., ".pkg")]' 2>/dev/null | sed -e 's/<Key>//g' -e 's/<\/Key>//g' | sort -V | tail -n 1 | sed 's/\/GlobalProtect\.pkg$//')
     rawURL=$(curl -fs https://pan-gp-client.s3.amazonaws.com/ | xpath '//ListBucketResult/Contents/Key[contains(., ".pkg")]' 2>/dev/null | sed -e 's/<Key>//g' -e 's/<\/Key>//g' | sort -V | tail -n 1)
     downloadURL="https://pan-gp-client.s3.amazonaws.com/${rawURL}"
     expectedTeamID="PXPZ95SK77"
     blockProcesses="GlobalProtect"
     packageID="come.paloaltonetworks.globalprotect.pkg"
     ;;
-    
